028880272fbe19c510f780f2aa20b35f4aa6d8c9
owlflyfine <owlflyfine@gmail.com>
2022-03-26 11:35:10 -0400

    feat: green case loop 1-100

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -12,3 +12,22 @@ export const fizzBuzz = (number) => {
   if (isBuzz(number)) return "Buzz";
   return number;
 };
+
+export const rangeFizzBuzz = () => {
+  let result = "";
+  const space = " ";
+  for (let number = 1; number <= 100; number++) {
+    let print = "";
+    if (isFizz(number) && isBuzz(number)) {
+      print += "FizzBuzz";
+    } else if (isFizz(number)) {
+      print += "Fizz";
+    } else if (isBuzz(number)) {
+      print += "Buzz";
+    } else {
+      print = number.toString();
+    }
+    result += space + print;
+  }
+  return result.trim();
+};
