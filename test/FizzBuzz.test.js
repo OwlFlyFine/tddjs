54eb983a5d3cea02600ce04cb75cd6ece134a10d
owlflyfine <owlflyfine@gmail.com>
2022-03-26 11:22:55 -0400

    feat: red case divide by 3

--- test/FizzBuzz.test.js
+++ test/FizzBuzz.test.js
@@ -6,3 +6,10 @@ test("Should return number that not divide by 3 and 5 ", () => {
   expect(fizzBuzz(4)).toBe(4);
   expect(fizzBuzz(11)).toBe(11);
 });
+
+test("Should return Fizz if divide by 3", () => {
+  expect(fizzBuzz(3)).toBe("Fizz");
+  expect(fizzBuzz(6)).toBe("Fizz");
+  expect(fizzBuzz(9)).toBe("Fizz");
+  expect(fizzBuzz(21)).toBe("Fizz");
+});
